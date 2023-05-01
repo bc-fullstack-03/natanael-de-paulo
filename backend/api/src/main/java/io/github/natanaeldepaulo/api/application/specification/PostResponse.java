982ad12a144e2df4f74aa5bafb7b443489eb3f10
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-04-30 23:52:43 -0300

    feat: implemented post creation route, crontroller, service, repository

--- backend/api/src/main/java/io/github/natanaeldepaulo/api/application/specification/PostResponse.java
+++ backend/api/src/main/java/io/github/natanaeldepaulo/api/application/specification/PostResponse.java
@@ -1,25 +1,25 @@
-package io.github.natanaeldepaulo.api.domain.entities;
+package io.github.natanaeldepaulo.api.application.specification;
 
 import io.github.natanaeldepaulo.api.domain.embedded.Comment;
 import io.github.natanaeldepaulo.api.domain.embedded.Likes;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
-@Document(collection = "posts")
 @Data
-public class Post {
-    @Id private final UUID id;
+public class PostResponse {
+    private UUID id;
     private String title;
     private String description;
     private Boolean image = false;
     private String imageUrl;
-    private UUID profile_id;;
+    private UUID profile_id;
 
-    public Post(String title, String description, Boolean image, String imageUrl, UUID profile_id){
-        this.id = UUID.randomUUID();
+
+    public PostResponse(UUID id, String title, String description, Boolean image, String imageUrl, UUID profile_id) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.image = image;
