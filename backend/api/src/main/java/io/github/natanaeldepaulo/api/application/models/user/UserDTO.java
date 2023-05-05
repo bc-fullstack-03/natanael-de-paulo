67b5fa2b3d37f8f73554fe175ad7392e7c16a83d
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-05-05 16:42:02 -0300

    feat: create map objet user

--- backend/api/src/main/java/io/github/natanaeldepaulo/api/application/models/user/UserDTO.java
+++ backend/api/src/main/java/io/github/natanaeldepaulo/api/application/models/user/UserDTO.java
@@ -3,21 +3,15 @@ package io.github.natanaeldepaulo.api.application.specification;
 import io.github.natanaeldepaulo.api.domain.embedded.Profile;
 import io.github.natanaeldepaulo.api.domain.entities.User;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
 @Data
-public class UserResponse {
+@NoArgsConstructor
+public class UserDTO {
     public UUID id;
     public String name;
     public String email;
     public Profile profile;
-
-    public UserResponse(){}
-
-    public UserResponse(User user){
-        this.id = user.getId();
-        this.name = user.getName();
-        this.email = user.getEmail();
-        this.profile = user.getProfile();
-    }
 }
